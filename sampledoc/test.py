f8f2749dccce5dd9e05e755e9bb6b5b1373a325c
Deekshat13 <143375745+Deekshat13@users.noreply.github.com>
2023-09-19 19:55:04 +0530

    Create test.py

---
+++ sampledoc/test.py
@@ -0,0 +1,13 @@
+ef check_number(number):
+    # Check if the score is more than or equal to 70
+    if number >= 70:
+        return "You have passed"
+    else:
+        return "You have not passed"
+
+# Define the number
+number = 70
+
+# Call the function to check the result and print it
+result = check_number(number)
+print(result)
